98a325f57b94b7236db37011245dca578beee92a
Nurwidi Amiryunanto <100020450+Nurwidi130603@users.noreply.github.com>
2025-05-27 15:07:37 +0700

    Update api.py
    Referensi:
    https://www.youtube.com/watch?v=bluclMxiUkA

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,9 @@
+import numpy as np
+from flask import Flask, request, render_template
+#import ml_pneumonia
 
+#Create an app object using the Flask class. 
+app = Flask(__name__)
+
+#Load the trained model. (Pickle file)
+model = pneumonia.load(open('models/model.pnm', 'rb'))
